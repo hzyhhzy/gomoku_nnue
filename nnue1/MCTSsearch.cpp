60068fdd0f503f9f2a1ad17e6911ba4a07f01402
dhbloo <duanhb1084@qq.com>
2021-12-15 19:56:54 +0800

    reduce size of MCTSchild to 8 bytes; format code

--- nnue1/MCTSsearch.cpp
+++ nnue1/MCTSsearch.cpp
@@ -1,108 +1,111 @@
 #include "MCTSsearch.h"
 
-MCTSnode::MCTSnode(Evaluator* evaluator, Color nextColor,double policyTemp, Loc* locbuf, PolicyType* pbuf1, PolicyType* pbuf2, float* pbuf3) :nextColor(nextColor)
+MCTSnode::MCTSnode(Evaluator * evaluator,
+                   Color       nextColor,
+                   double      policyTemp,
+                   Loc *       locbuf,
+                   PolicyType *pbuf1,
+                   PolicyType *pbuf2,
+                   float *     pbuf3)
+    : nextColor(nextColor)
 {
-  sureResult = MC_UNCERTAIN;
+  sureResult  = MC_UNCERTAIN;
   childrennum = 0;
-  for (int i = 0; i < MAX_MCTS_CHILDREN; i++)children[i].ptr = NULL;
+  for (int i = 0; i < MAX_MCTS_CHILDREN; i++)
+    children[i].setPtr(nullptr);
   visits = 1;
 
-  //calculate policy
+  // calculate policy
   WRtotal = evaluator->evaluateFull(nextColor, pbuf1).winlossrate();
-  for (Loc loc = 0; loc < BS * BS; loc++)
-  {
-    if (evaluator->board[loc] != C_EMPTY)pbuf1[loc] = MIN_POLICY;
+  for (Loc loc = 0; loc < BS * BS; loc++) {
+    if (evaluator->board[loc] != C_EMPTY)
+      pbuf1[loc] = MIN_POLICY;
   }
 
-  //policy sort
+  // policy sort
   std::iota(locbuf, locbuf + BS * BS, LOC_ZERO);
-  std::sort(locbuf, locbuf + BS * BS, [&](Loc a, Loc b) {
-    return pbuf1[a] > pbuf1[b];
-    });
+  std::sort(locbuf, locbuf + BS * BS, [&](Loc a, Loc b) { return pbuf1[a] > pbuf1[b]; });
 
-  //count legal moves
+  // count legal moves
   legalChildrennum = MAX_MCTS_CHILDREN;
-  for (int i = 0; i < MAX_MCTS_CHILDREN; i++)
-  {
+  for (int i = 0; i < MAX_MCTS_CHILDREN; i++) {
     pbuf2[i] = pbuf1[locbuf[i]];
-    if (evaluator->board[locbuf[i]] != C_EMPTY)
-    {
+    if (evaluator->board[locbuf[i]] != C_EMPTY) {
       legalChildrennum = i;
       break;
     }
   }
 
-  //check draw
-  if (legalChildrennum == 0)
-  {
-
+  // check draw
+  if (legalChildrennum == 0) {
     sureResult = MC_DRAW;
     return;
   }
 
-
   PolicyType maxRawPolicy = *std::max_element(pbuf2, pbuf2 + legalChildrennum);
-  std::transform(pbuf2, pbuf2 + legalChildrennum, pbuf3, [=](auto& p) {
+  std::transform(pbuf2, pbuf2 + legalChildrennum, pbuf3, [=](auto &p) {
     const double invQ = 1.0 / quantFactor / policyTemp;
     return (float)std::exp((p - maxRawPolicy) * invQ);
     // return (float)std::max(p, PolicyType(0));
-    });
+  });
   float policySum = std::reduce(pbuf3, pbuf3 + legalChildrennum);
-  float k = 1 / policySum;
-  std::transform(pbuf3, pbuf3 + legalChildrennum, pbuf3, [=](auto& p) {
-    return p * k;
-    });
+  float k         = 1 / policySum;
+  std::transform(pbuf3, pbuf3 + legalChildrennum, pbuf3, [=](auto &p) { return p * k; });
 
-  for (int i = 0; i < legalChildrennum; i++)
-  {
-    children[i].loc = locbuf[i];
+  for (int i = 0; i < legalChildrennum; i++) {
+    children[i].loc    = locbuf[i];
     children[i].policy = uint16_t(pbuf3[i] * policyQuant) + 1;
   }
 }
 
-MCTSnode::MCTSnode(MCTSsureResult sureResult, Color nextColor) :nextColor(nextColor), sureResult(sureResult)
+MCTSnode::MCTSnode(MCTSsureResult sureResult, Color nextColor)
+    : nextColor(nextColor)
+    , sureResult(sureResult)
 {
-  visits = 1;
-  WRtotal = sureResultWR(sureResult);
-  childrennum = 0;
+  visits           = 1;
+  WRtotal          = sureResultWR(sureResult);
+  childrennum      = 0;
   legalChildrennum = 0;
 }
 
 MCTSnode::~MCTSnode()
 {
-  for (int i = 0; i < childrennum; i++)
-  {
-    if (children[i].ptr != NULL)delete children[i].ptr;
+  for (int i = 0; i < childrennum; i++) {
+    if (children[i].ptr() != NULL)
+      delete children[i].ptr();
   }
 }
 
-MCTSsearch::MCTSsearch(Evaluator* e) :Search(e), rootNode(NULL), vcfSolver{ {BS, BS, C_BLACK}, {BS, BS, C_WHITE} }
+MCTSsearch::MCTSsearch(Evaluator *e)
+    : Search(e)
+    , rootNode(NULL)
+    , vcfSolver {{BS, BS, C_BLACK}, {BS, BS, C_WHITE}}
 {
   vcfSolver[0].setBoard(boardPointer, false, true);
   vcfSolver[1].setBoard(boardPointer, false, true);
 }
 
-float MCTSsearch::fullsearch(Color color, double factor, Loc& bestmove)
+float MCTSsearch::fullsearch(Color color, double factor, Loc &bestmove)
 {
   vcfSolver[0].setBoard(boardPointer, false, true);
   vcfSolver[1].setBoard(boardPointer, false, true);
 
-  //check VCF
-  VCF::SearchResult VCFresult=vcfSolver[color - 1].fullSearch(10000, 10, bestmove, false);
-  if (VCFresult == VCF::SR_Win)
-  {
+  // check VCF
+  VCF::SearchResult VCFresult =
+      vcfSolver[color - 1].fullSearch(10000, 10, bestmove, false);
+  if (VCFresult == VCF::SR_Win) {
     //直接vcf，不需要mcts
     return 1;
   }
 
-
-  if (factor != 0)option.maxNodes = factor;
+  if (factor != 0)
+    option.maxNodes = factor;
   terminate = false;
 
-  //if root node is NULL, create root node
-  if (rootNode == NULL)
-  {
-    rootNode=new MCTSnode(evaluator, color,params.policyTemp, locbuf, pbuf1, pbuf2, pbuf3);
+  // if root node is NULL, create root node
+  if (rootNode == NULL) {
+    rootNode =
+        new MCTSnode(evaluator, color, params.policyTemp, locbuf, pbuf1, pbuf2, pbuf3);
   }
   search(rootNode, option.maxNodes, true);
   bestmove = bestRootMove();
@@ -115,40 +118,41 @@ void MCTSsearch::play(Color color, Loc loc)
   vcfSolver[0].playOutside(loc, color, 1, true);
   vcfSolver[1].playOutside(loc, color, 1, true);
 
-
   //树重用
 
-  //if (rootNode != NULL)delete rootNode;
-  //rootNode = NULL;
-  //return;
+  // if (rootNode != NULL)delete rootNode;
+  // rootNode = NULL;
+  // return;
 
-
-  if (rootNode != NULL)
-  {
-    //VCF没保存后续招法，所以删掉根节点重新计算
-    if (rootNode->sureResult != MC_UNCERTAIN) { delete rootNode; rootNode = NULL; }
+  if (rootNode != NULL) {
+    // VCF没保存后续招法，所以删掉根节点重新计算
+    if (rootNode->sureResult != MC_UNCERTAIN) {
+      delete rootNode;
+      rootNode = NULL;
+    }
     //落子方不对，所以重新计算
-    else if (rootNode->nextColor != color) { delete rootNode; rootNode = NULL; }
+    else if (rootNode->nextColor != color) {
+      delete rootNode;
+      rootNode = NULL;
+    }
     //检查是否存在对应的子分支
-    else
-    {
+    else {
       bool haveThisChild = false;
-      for (int i = 0; i < rootNode->childrennum; i++)
-      {
-        if (rootNode->children[i].loc == loc)
-        {
-          haveThisChild = true;
-          MCTSnode* nextRootNode = rootNode->children[i].ptr;
-          rootNode->children[i].ptr = NULL;//防止被rootNode的析构函数删除。
+      for (int i = 0; i < rootNode->childrennum; i++) {
+        if (rootNode->children[i].loc == loc) {
+          haveThisChild          = true;
+          MCTSnode *nextRootNode = rootNode->children[i].ptr();
+          rootNode->children[i].setPtr(nullptr);  //防止被rootNode的析构函数删除。
           delete rootNode;
           rootNode = nextRootNode;
           //新的rootNode可能是叶子节点，所以检查一下
-          if (rootNode->sureResult != MC_UNCERTAIN) { haveThisChild=false; }
+          if (rootNode->sureResult != MC_UNCERTAIN) {
+            haveThisChild = false;
+          }
           break;
         }
       }
-      if (!haveThisChild)
-      {
+      if (!haveThisChild) {
         delete rootNode;
         rootNode = NULL;
       }
@@ -168,7 +172,8 @@ void MCTSsearch::undo(Loc loc)
 void MCTSsearch::clearBoard()
 {
   evaluator->clear();
-  if (rootNode != NULL)delete rootNode;
+  if (rootNode != NULL)
+    delete rootNode;
   rootNode = NULL;
 }
 
@@ -176,7 +181,8 @@ void MCTSsearch::loadParamFile(std::string filename)
 {
   using namespace std;
   ifstream fs(filename);
-  if (!fs.good())return;
+  if (!fs.good())
+    return;
 
   string varname;
 
@@ -187,15 +193,12 @@ void MCTSsearch::loadParamFile(std::string filename)
   }
   fs >> params.expandFactor;
 
-
   fs >> varname;
   if (varname != "puct") {
     cout << "Wrong parameter name 2:" << varname << endl;
     return;
   }
   fs >> params.puct;
-
-
 
   fs >> varname;
   if (varname != "puctPow") {
@@ -204,7 +207,6 @@ void MCTSsearch::loadParamFile(std::string filename)
   }
   fs >> params.puctPow;
 
-
   fs >> varname;
   if (varname != "puctBase") {
     cout << "Wrong parameter name 4:" << varname << endl;
@@ -212,14 +214,12 @@ void MCTSsearch::loadParamFile(std::string filename)
   }
   fs >> params.puctBase;
 
-
   fs >> varname;
   if (varname != "fpuReduction") {
     cout << "Wrong parameter name 5:" << varname << endl;
     return;
   }
   fs >> params.fpuReduction;
-
 
   fs >> varname;
   if (varname != "policyTemp") {
@@ -243,55 +243,57 @@ void MCTSsearch::undoForSearch(Loc loc)
   vcfSolver[1].undoOutside(loc, 1);
 }
 
-MCTSsearch::SearchResult MCTSsearch::search(MCTSnode* node, uint64_t remainVisits, bool isRoot)
+MCTSsearch::SearchResult
+MCTSsearch::search(MCTSnode *node, uint64_t remainVisits, bool isRoot)
 {
-  if (remainVisits == 0)remainVisits = INT64_MAX;
+  if (remainVisits == 0)
+    remainVisits = INT64_MAX;
 
   //正常情况是一次playout一个visit，为了降低开销直接让visit变成（1+expandFactor）倍，新增expandFactor倍的visit
-  if (!isRoot)remainVisits = std::min(remainVisits, uint64_t(params.expandFactor * double(node->visits))+1);
+  if (!isRoot)
+    remainVisits =
+        std::min(remainVisits, uint64_t(params.expandFactor * double(node->visits)) + 1);
 
-  SearchResult SR = { 0,0 };
+  SearchResult SR = {0, 0};
 
-  if (node->sureResult != MC_UNCERTAIN)
-  {
+  if (node->sureResult != MC_UNCERTAIN) {
     node->visits += remainVisits;
     SR.newVisits = remainVisits;
-    SR.WRchange = sureResultWR(node->sureResult) * remainVisits;
+    SR.WRchange  = sureResultWR(node->sureResult) * remainVisits;
     node->WRtotal += SR.WRchange;
     return SR;
   }
 
   Color color = node->nextColor;
-  Color opp = getOpp(color);
-  while (remainVisits > 0 && !terminate)
-  {
-    int nextChildID=selectChildIDToSearch(node);
-    Loc nextChildLoc = node->children[nextChildID].loc;
+  Color opp   = getOpp(color);
+  while (remainVisits > 0 && !terminate) {
+    int          nextChildID  = selectChildIDToSearch(node);
+    Loc          nextChildLoc = node->children[nextChildID].loc;
     SearchResult childSR;
-    if (nextChildID >= node->childrennum)//new child
+    if (nextChildID >= node->childrennum)  // new child
     {
       node->childrennum++;
-      MCTSsureResult sr=checkSureResult(nextChildLoc, color);
-      if (sr != MC_UNCERTAIN)node->children[nextChildID].ptr = new MCTSnode(sr, opp);
-      else
-      {
+      MCTSsureResult sr = checkSureResult(nextChildLoc, color);
+      if (sr != MC_UNCERTAIN)
+        node->children[nextChildID].setPtr(new MCTSnode(sr, opp));
+      else {
         playForSearch(color, nextChildLoc);
-        node->children[nextChildID].ptr = new MCTSnode(evaluator, opp,params.policyTemp, locbuf, pbuf1, pbuf2, pbuf3);
+        node->children[nextChildID].setPtr(
+            new MCTSnode(evaluator, opp, params.policyTemp, locbuf, pbuf1, pbuf2, pbuf3));
         undoForSearch(nextChildLoc);
       }
 
       childSR.newVisits = 1;
-      childSR.WRchange = node->children[nextChildID].ptr->WRtotal;
+      childSR.WRchange  = node->children[nextChildID].ptr()->WRtotal;
     }
-    else
-    {
+    else {
       playForSearch(color, nextChildLoc);
-      childSR = search(node->children[nextChildID].ptr, remainVisits, false);
+      childSR = search(node->children[nextChildID].ptr(), remainVisits, false);
       undoForSearch(nextChildLoc);
     }
-    //update stats
+    // update stats
     remainVisits -= childSR.newVisits;
-    //std::cout << "debug: " << childSR.newVisits << " " << childSR.WRchange<<"\n";
+    // std::cout << "debug: " << childSR.newVisits << " " << childSR.WRchange<<"\n";
     node->visits += childSR.newVisits;
     node->WRtotal -= childSR.WRchange;
     SR.newVisits += childSR.newVisits;
@@ -303,79 +305,77 @@ MCTSsearch::SearchResult MCTSsearch::search(MCTSnode* node, uint64_t remainVisit
 
 MCTSsureResult MCTSsearch::checkSureResult(Loc nextMove, Color color)
 {
-  //return MC_UNCERTAIN;//to disable VCF
-  
-  //evaluator没有play，因为evaluator play的开销较大
+  // return MC_UNCERTAIN;//to disable VCF
+
+  // evaluator没有play，因为evaluator play的开销较大
   Color opp = getOpp(color);
-  vcfSolver[opp-1].playOutside(nextMove, color, 1, true);
-  Loc vcfloc;
-  VCF::SearchResult sr=vcfSolver[opp - 1].fullSearch(5000, 4, vcfloc, false);
-  vcfSolver[opp-1].undoOutside(nextMove, 1);
+  vcfSolver[opp - 1].playOutside(nextMove, color, 1, true);
+  Loc               vcfloc;
+  VCF::SearchResult sr = vcfSolver[opp - 1].fullSearch(5000, 4, vcfloc, false);
+  vcfSolver[opp - 1].undoOutside(nextMove, 1);
 
-  if (sr == VCF::SR_Win)return MC_Win;
+  if (sr == VCF::SR_Win)
+    return MC_Win;
   return MC_UNCERTAIN;
-
 }
 
-int MCTSsearch::selectChildIDToSearch(MCTSnode* node)
+int MCTSsearch::selectChildIDToSearch(MCTSnode *node)
 {
   int childrennum = node->childrennum;
-  if (childrennum == 0)return 0;
+  if (childrennum == 0)
+    return 0;
 
   double bestSelectionValue = -1e20;
-  int bestChildID = -1;
+  int    bestChildID        = -1;
 
   double totalVisit = node->visits;
-  double puctFactor = MCTSpuctFactor(totalVisit, params.puct, params.puctPow,params.puctBase);
-  
-  double totalChildPolicy=0;
-  for (int i = 0; i < childrennum; i++)
-  {
-    const MCTSnode* child = node->children[i].ptr;
-    double visit = child->visits;
-    double value = -child->WRtotal / visit;
-    double policy = double(node->children[i].policy) *policyQuantInv ;
+  double puctFactor =
+      MCTSpuctFactor(totalVisit, params.puct, params.puctPow, params.puctBase);
+
+  double totalChildPolicy = 0;
+  for (int i = 0; i < childrennum; i++) {
+    const MCTSnode *child  = node->children[i].ptr();
+    double          visit  = child->visits;
+    double          value  = -child->WRtotal / visit;
+    double          policy = double(node->children[i].policy) * policyQuantInv;
     totalChildPolicy += policy;
 
     double selectionValue = MCTSselectionValue(puctFactor, value, visit, policy);
-    if (selectionValue > bestSelectionValue)
-    {
+    if (selectionValue > bestSelectionValue) {
       bestSelectionValue = selectionValue;
-      bestChildID = i;
+      bestChildID        = i;
     }
-
-
   }
 
-  //check new child
-  if(childrennum<node->legalChildrennum)
-  {
-    double value = node->WRtotal / totalVisit - sqrt(totalChildPolicy) * params.fpuReduction;
-    double policy = double(node->children[childrennum].policy) * policyQuantInv;
-    double visit = 0;
+  // check new child
+  if (childrennum < node->legalChildrennum) {
+    double value =
+        node->WRtotal / totalVisit - sqrt(totalChildPolicy) * params.fpuReduction;
+    double policy         = double(node->children[childrennum].policy) * policyQuantInv;
+    double visit          = 0;
     double selectionValue = MCTSselectionValue(puctFactor, value, visit, policy);
-    if (selectionValue > bestSelectionValue)bestChildID = childrennum;
+    if (selectionValue > bestSelectionValue)
+      bestChildID = childrennum;
   }
 
   return bestChildID;
-
-
 }
 
 Loc MCTSsearch::bestRootMove() const
 {
-  if (rootNode == NULL)return LOC_NULL;
-  if (rootNode->legalChildrennum <= 0)return LOC_NULL;
-  if (rootNode->childrennum <= 0)return rootNode->children[0].loc;
+  if (rootNode == NULL)
+    return LOC_NULL;
+  if (rootNode->legalChildrennum <= 0)
+    return LOC_NULL;
+  if (rootNode->childrennum <= 0)
+    return rootNode->children[0].loc;
   uint64_t bestVisit = 0;
-  Loc bestLoc = LOC_NULL;
-  for (int i = 0; i < rootNode->childrennum; i++)
-  {
-    uint64_t visit = rootNode->children[i].ptr->visits;
-    if (visit > bestVisit)
-    {
+  Loc      bestLoc   = LOC_NULL;
+  for (int i = 0; i < rootNode->childrennum; i++) {
+    uint64_t visit = rootNode->children[i].ptr()->visits;
+    if (visit > bestVisit) {
       bestVisit = visit;
-      bestLoc = rootNode->children[i].loc;
+      bestLoc   = rootNode->children[i].loc;
     }
   }
   return bestLoc;
@@ -383,12 +383,14 @@ Loc MCTSsearch::bestRootMove() const
 
 float MCTSsearch::getRootValue() const
 {
-  if (rootNode == NULL)return 0;
+  if (rootNode == NULL)
+    return 0;
   return rootNode->WRtotal / double(rootNode->visits);
 }
 
 int64_t MCTSsearch::getRootVisit() const
 {
-  if (rootNode == NULL)return 0;
+  if (rootNode == NULL)
+    return 0;
   return rootNode->visits;
 }
--- nnue1/MCTSsearch.h
+++ nnue1/MCTSsearch.h
@@ -1,118 +1,160 @@
 #pragma once
 #include "Search.h"
 #include "VCF/VCFsolver.h"
-const int MAX_MCTS_CHILDREN = 50;
-const double policyQuant = 50000;
-const double policyQuantInv = 1/policyQuant;
+const int    MAX_MCTS_CHILDREN = 50;
+const double policyQuant       = 50000;
+const double policyQuantInv    = 1 / policyQuant;
 
-enum MCTSsureResult : int16_t {
-  MC_Win = 1,
-  MC_LOSE = -1,
-  MC_DRAW = 2,
-  MC_UNCERTAIN = 0
-};
+enum MCTSsureResult : int16_t { MC_Win = 1, MC_LOSE = -1, MC_DRAW = 2, MC_UNCERTAIN = 0 };
 inline double sureResultWR(MCTSsureResult sr)
 {
-  if (sr == MC_Win)return 1;
-  else if (sr == MC_LOSE)return -1;
-  else return 0;
+  if (sr == MC_Win)
+    return 1;
+  else if (sr == MC_LOSE)
+    return -1;
+  else
+    return 0;
 }
 
-inline double MCTSpuctFactor(double totalVisit, double puct, double puctPow, double puctBase)
+inline double
+MCTSpuctFactor(double totalVisit, double puct, double puctPow, double puctBase)
 {
-  return  puct * pow((totalVisit+puctBase)/puctBase, puctPow);
+  return puct * pow((totalVisit + puctBase) / puctBase, puctPow);
 }
 
-inline double MCTSselectionValue(double puctFactor, double value,double childVisit,double childPolicy)
+inline double
+MCTSselectionValue(double puctFactor, double value, double childVisit, double childPolicy)
 {
   return value + puctFactor * childPolicy / (childVisit + 1);
 }
 
 struct MCTSnode;
 
+struct int128_t
+{
+  int64_t data[2];
+};
+
 struct MCTSchild
 {
-  MCTSnode* ptr;
-  Loc loc;
-  uint16_t policy;//除以policyQuant是原始policy
+  int32_t  offset;
+  Loc      loc;
+  uint16_t policy;  // 除以policyQuant是原始policy
+
+  MCTSnode *ptr() const
+  {
+    if (offset)
+      return reinterpret_cast<MCTSnode *>(base_ptr() + offset);
+    else
+      return nullptr;
+  }
+  void setPtr(MCTSnode *ptr)
+  {
+    // 对于空指针，offset直接存0
+    if (ptr == nullptr) {
+      offset = 0;
+      return;
+    }
+
+    // 由于MCTSnode已经是16byte对齐的，该转换没问题
+    int128_t *target = reinterpret_cast<int128_t *>(ptr);
+    ptrdiff_t diff   = target - base_ptr();
+    offset           = (int32_t)diff;
+    // 由于内存是连续分配的，两次分配的距离超过32位offset表示范围:
+    // 2^32 * sizeof(int128_t) = 64GB 的情况不可能发生
+    // 但最好还是检测一下
+    if (offset != diff) {
+      std::cerr << "You are very unlucky!!" << std::endl;
+      std::terminate();
+    }
+  }
+
+private:
+  // 获得对齐到int64_t的基准指针
+  int128_t *base_ptr() const
+  {
+    intptr_t this_ptr = reinterpret_cast<intptr_t>(this);
+    return reinterpret_cast<int128_t *>(this_ptr & (~15));
+  }
 };
 
-struct MCTSnode
+struct alignas(int128_t) MCTSnode
 {
   MCTSsureResult sureResult;
-  int16_t childrennum;
-  int16_t legalChildrennum;//=min(合法招法，MAX_MCTS_CHILDREN)
+  int16_t        childrennum;
+  int16_t        legalChildrennum;  //=min(合法招法，MAX_MCTS_CHILDREN)
+  Color          nextColor;
+  uint64_t       visits;  //包括自己
+  double WRtotal;  //以下一次落子的这一方的视角,1胜-1负, 平均胜率=WRtotal/visits
   MCTSchild children[MAX_MCTS_CHILDREN];
-  
 
-  uint64_t visits;//包括自己
-  double WRtotal;//以下一次落子的这一方的视角,1胜-1负
-  //平均胜率=WRtotal/visits
-
-  Color nextColor;
-  
-  MCTSnode(Evaluator* evaluator,  Color nextColor,double policyTemp,Loc* locbuf,PolicyType* pbuf1,PolicyType* pbuf2,float* pbuf3);
+  MCTSnode(Evaluator * evaluator,
+           Color       nextColor,
+           double      policyTemp,
+           Loc *       locbuf,
+           PolicyType *pbuf1,
+           PolicyType *pbuf2,
+           float *     pbuf3);
   MCTSnode(MCTSsureResult sureResult, Color nextColor);
   ~MCTSnode();
-  
 };
 
-class MCTSsearch :
-    public Search
+const int a = sizeof(MCTSnode);
+
+class MCTSsearch : public Search
 {
 public:
-  MCTSnode* rootNode;
+  MCTSnode *rootNode;
 
   struct Option
   {
-    size_t maxNodes=0;
+    size_t maxNodes = 0;
   } option;
   struct Param
   {
     double expandFactor = 0.4;
-    double puct = 0.6;
-    double puctPow = 0.7;//传统的mcts对应0.5
-    double puctBase = 1.0;
+    double puct         = 0.6;
+    double puctPow      = 0.7;  //传统的mcts对应0.5
+    double puctBase     = 1.0;
     double fpuReduction = 0.1;
-    double policyTemp = 1.1;
+    double policyTemp   = 1.1;
 
-  }params;
+  } params;
 
   VCFsolver vcfSolver[2];
 
-  Loc locbuf[BS * BS];
+  Loc        locbuf[BS * BS];
   PolicyType pbuf1[BS * BS], pbuf2[MAX_MCTS_CHILDREN];
-  float pbuf3[MAX_MCTS_CHILDREN];
-
+  float      pbuf3[MAX_MCTS_CHILDREN];
 
   MCTSsearch(Evaluator *e);
-  virtual float    fullsearch(Color color, double factor, Loc &bestmove);
-  virtual void play(Color color, Loc loc);
-  virtual void undo( Loc loc);
-  virtual void clearBoard(); 
+  virtual float fullsearch(Color color, double factor, Loc &bestmove);
+  virtual void  play(Color color, Loc loc);
+  virtual void  undo(Loc loc);
+  virtual void  clearBoard();
 
-  Loc bestRootMove() const;
-  float getRootValue() const;
+  Loc     bestRootMove() const;
+  float   getRootValue() const;
   int64_t getRootVisit() const;
 
-  void             setOptions(size_t maxNodes) { option.maxNodes = maxNodes; }
+  void setOptions(size_t maxNodes) { option.maxNodes = maxNodes; }
   void loadParamFile(std::string filename);
-  ~MCTSsearch() { if(rootNode!=NULL)delete rootNode; }
+  ~MCTSsearch()
+  {
+    if (rootNode != NULL)
+      delete rootNode;
+  }
 
 private:
   struct SearchResult
   {
     uint64_t newVisits;
-    double WRchange;
+    double   WRchange;
   };
   void playForSearch(Color color, Loc loc);
   void undoForSearch(Loc loc);
 
-  SearchResult search(MCTSnode* node, uint64_t remainVisits,bool isRoot);
-  MCTSsureResult checkSureResult(Loc nextMove, Color color);//check VCF
-  int selectChildIDToSearch(MCTSnode* node);
-
-
-
+  SearchResult   search(MCTSnode *node, uint64_t remainVisits, bool isRoot);
+  MCTSsureResult checkSureResult(Loc nextMove, Color color);  // check VCF
+  int            selectChildIDToSearch(MCTSnode *node);
 };
-
